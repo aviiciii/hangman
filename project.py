7ee86538adaeff9966a2e989992fd09d65007732
aviiciii <laavesh1@gmail.com>
2022-08-31 23:01:34 +0530

    trying apis

--- project.py
+++ project.py
@@ -1,11 +1,29 @@
+import requests
+import re
 
+top250_url = "http://akas.imdb.com/chart/top"
+
+
+def get_top250():
+    r = requests.get(top250_url)
+    html = r.text.split("\n")
+    result = []
+    for line in html:
+        line = line.rstrip("\n")
+        m = re.search(r'data-titleid="tt(\d+?)">', line)
+        if m:
+            _id = m.group(1)
+            result.append(_id)
+    #
+    return result
 
 
 def main():
-    ...
+    print(get_top250())
 
 
-def function_1():
+
+def f():
     ...
 
 
